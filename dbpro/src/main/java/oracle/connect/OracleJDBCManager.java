45059a9d6e8b8eef222a7a5b964e92ba2c6fc831
hahava <hahava5696@gmail.com>
2020-03-17 11:33:17 +0900

    Remove unused methods

--- dbpro/src/main/java/oracle/connect/OracleJDBCManager.java
+++ dbpro/src/main/java/oracle/connect/OracleJDBCManager.java
@@ -1,10 +1,8 @@
 package oracle.connect;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class OracleJDBCManager {
 	public void registerOracleJDBCDriver() {
@@ -35,31 +33,6 @@ public class OracleJDBCManager {
 		} else {
 			System.out.println("Failed to make connection!");
 			return null;
-		}
-	}
-
-	public ResultSet executeSelectQuery(Connection conn, String query) {
-		try {
-			Statement stmt = conn.createStatement();
-			ResultSet rset = stmt.executeQuery("select * from tab");
-			return rset;
-		} catch (SQLException e) {
-			System.out.println(e);
-			return null;
-		}
-	}
-
-	public void printResult(ResultSet rset) {
-		/*
-		 * In this situation, I print the only first column in result.
-		 */
-		try {
-			while (rset.next()) {
-				System.out.println(rset.getString(1));
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 }
