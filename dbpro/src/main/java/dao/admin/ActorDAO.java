7ec1a0d26862a7914390f23f8e6564f673aab7ca
hahava <hahava5696@gmail.com>
2020-03-17 20:29:55 +0900

    Refactor get actor list method

--- dbpro/src/main/java/dao/admin/ActorDAO.java
+++ dbpro/src/main/java/dao/admin/ActorDAO.java
@@ -1,14 +1,14 @@
 package dao.admin;
 
+import dto.admin.ActorDTO;
+import oracle.connect.JDBCManager;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-import dto.admin.ActorDTO;
-import oracle.connect.*;
 
 public class ActorDAO implements DAO {
 
@@ -26,16 +26,22 @@ public class ActorDAO implements DAO {
 	private ActorDAO() {
 	}
 
-	// 객체를 생성한다.
-	private static ActorDAO instance = new ActorDAO();
+	private static ActorDAO instance;
 
-	// 객체를 반환하다.
 	public static ActorDAO getInstance() {
+		if (instance == null) {
+			instance = new ActorDAO();
+		}
 		return instance;
 	}
 
 	// 등록된 배우를 전부 출력한다.
 	public void list() {
+		final String query = "SELECT ACTOR_CODE, ACTOR_NAME, ACTOR_GENDER, ACTOR_BIRTH FROM ACTOR";
+		JDBCManager
+			.getInstance()
+			.queryForList(query, ActorDTO.class)
+			.forEach(actorDTO -> System.out.println(actorDTO.toString()));
 	}
 
 	// 특정 영화에 출연한 배우 검색
@@ -55,20 +61,20 @@ public class ActorDAO implements DAO {
 		movieName = sc.nextLine();
 
 		/*
-		 * 영화 이름으로 검색할 것이기 때문에 movie 테이블에서 해당 영화이름을 가진 영화의 코드를 받아온 뒤, movie_actor 테이블에서 해당 영화코드에 할당된 actor_code를 
+		 * 영화 이름으로 검색할 것이기 때문에 movie 테이블에서 해당 영화이름을 가진 영화의 코드를 받아온 뒤, movie_actor 테이블에서 해당 영화코드에 할당된 actor_code를
 		 * 받아오고, 그것을 기반으로 actor테이블에서 배우 정보를 얻어온다.
 		 * subquery를 이용했다.
 		 * */
 		String query = "select actor_code, actor_name,actor_gender, actor_birth from actor where actor_code in "
-				+ "(select actor_code from movie_actor where movie_code in " + "(select movie_code from movie where movie_title='" + movieName
-				+ "'))";
+			+ "(select actor_code from movie_actor where movie_code in " + "(select movie_code from movie where movie_title='" + movieName
+			+ "'))";
 
 		try {
 			pstm = conn.prepareStatement(query);
 			result = pstm.executeQuery();
 			while (result.next()) {
-//				actorByMovie.add(new ActorDTO(result.getString("actor_code"), result.getString("actor_name"), result.getString("actor_gender"),
-//						result.getString("actor_birth")));
+				//				actorByMovie.add(new ActorDTO(result.getString("actor_code"), result.getString("actor_name"), result.getString("actor_gender"),
+				//						result.getString("actor_birth")));
 			}
 			if (actorByMovie.size() == 0) {
 				System.out.println("그런 영화가 없거나 관리자가 배우정보를 등록하지 않았습니다.");
@@ -76,7 +82,7 @@ public class ActorDAO implements DAO {
 			}
 			System.out.println("배우이름\t배우생일\t배우성별");
 			for (int i = 0; i < actorByMovie.size(); i++) {
-//				System.out.println(actorByMovie.get(i).actorName + "\t" + actorByMovie.get(i).actorBirth + "\t" + actorByMovie.get(i).actorGender);
+				//				System.out.println(actorByMovie.get(i).actorName + "\t" + actorByMovie.get(i).actorBirth + "\t" + actorByMovie.get(i).actorGender);
 
 			}
 		} catch (SQLException e) {
--- dbpro/src/test/java/dao/ActorDAOTest.java
+++ dbpro/src/test/java/dao/ActorDAOTest.java
@@ -1,17 +1,13 @@
 package dao;
 
-import dto.admin.ActorDTO;
-import oracle.connect.JDBCManager;
+import dao.admin.ActorDAO;
 import org.junit.Test;
 
 public class ActorDAOTest {
 
 	@Test
 	public void getActorsTest() {
-		JDBCManager
-			.getInstance()
-			.queryForList("SELECT ACTOR_CODE, ACTOR_NAME, ACTOR_GENDER, ACTOR_BIRTH FROM ACTOR", ActorDTO.class)
-			.forEach(actorDTO -> System.out.println(actorDTO.toString()));
+		ActorDAO.getInstance().list();
 	}
 
 }
