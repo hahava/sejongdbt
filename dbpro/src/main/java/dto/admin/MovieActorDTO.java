fe1fa1143e65baf2c739e977df78f12ba663a9ea
hahava <hahava5696@gmail.com>
2020-03-17 18:03:27 +0900

    Add lombok

--- dbpro/src/main/java/dto/admin/MovieActorDTO.java
+++ dbpro/src/main/java/dto/admin/MovieActorDTO.java
@@ -1,30 +1,22 @@
 package dto.admin;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import oracle.connect.ColumnName;
 
-public class MovieActorDTO implements DTO{
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieActorDTO {
 
 	@ColumnName("ACTOR_CODE")
 	private String actorCode;
+
 	@ColumnName("MOVIE_CODE")
 	private String movieCode;
+
 	@ColumnName("ACTOR_ROLE")
 	private String actorRole;
-
-	@Override
-	public String toString() {
-		return "MovieActorDTO [ACTOR_CODE=" + actorCode + ", MOVIE_CODE=" + movieCode + ", ACTOR_ROLE=" + actorRole
-				+ "]";
-	}
-
-	public MovieActorDTO() {
-	}
-
-	public MovieActorDTO(String aCTOR_CODE, String mOVIE_CODE, String aCTOR_ROLE) {
-		super();
-		actorCode = aCTOR_CODE;
-		movieCode = mOVIE_CODE;
-		actorRole = aCTOR_ROLE;
-	}
 
 }
