24105d2773045cccd2323336446f42c94fd05188
hahava <hahava5696@gmail.com>
2020-03-18 10:35:05 +0900

    Refactor get advertisement method

--- dbpro/src/test/java/dao/AdDAOTest.java
+++ dbpro/src/test/java/dao/AdDAOTest.java
@@ -1,18 +1,13 @@
 package dao;
 
-import dto.admin.AdDTO;
-import oracle.connect.JDBCManager;
+import dao.admin.AdDAO;
 import org.junit.Test;
 
 public class AdDAOTest {
 
 	@Test
 	public void getAdListTest() {
-		final String query = "SELECT AD_TITLE, AD_COMPANY,AD_DATE, AD_PRICE FROM AD";
-		JDBCManager
-			.getInstance()
-			.queryForList(query, AdDTO.class)
-			.forEach(adDTO -> System.out.println(adDTO.toString()));
+		AdDAO.getInstance().list();
 	}
 
 }
