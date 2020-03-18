b5072bc3a878b9575eb189f8aa9a4af23acb2033
hahava <hahava5696@gmail.com>
2020-03-18 10:16:00 +0900

    Refactor getActor method

--- dbpro/src/main/java/dao/admin/ActorDAO.java
+++ dbpro/src/main/java/dao/admin/ActorDAO.java
@@ -2,31 +2,16 @@ package dao.admin;
 
 import dto.admin.ActorDTO;
 import oracle.connect.JDBCManager;
+import org.apache.commons.lang3.StringUtils;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
 
 public class ActorDAO implements DAO {
 
-	// driver를 이용하여 서버에 접속하는 메서드
-	private Connection getConnection() {
-		JDBCManager manager = new JDBCManager();
-		String oracleId = "s15010924";
-		String passwd = "s15010924";
-		int port = 1521;
-
-		Connection conn = manager.connect(oracleId, passwd, port);
-		return conn;
-	}
+	private static ActorDAO instance;
 
 	private ActorDAO() {
 	}
-
-	private static ActorDAO instance;
 
 	public static ActorDAO getInstance() {
 		if (instance == null) {
@@ -47,51 +32,33 @@ public class ActorDAO implements DAO {
 	// 특정 영화에 출연한 배우 검색
 	public void getActor(String movieTitle) {
 
-
-		Connection conn = getConnection();
-		PreparedStatement pstm = null;
-		ResultSet result = null;
-
-		ArrayList<ActorDTO> actorByMovie = new ArrayList<ActorDTO>();
-
 		System.out.println("특정 영화에 출연한 배우를 검색합니다. 영화 명을 입력하세요.");
 
+		final String query = new StringBuilder(
+			"SELECT AC.ACTOR_CODE AS ACTOR_CODE, AC.ACTOR_NAME AS ACTOR_NAME, AC.ACTOR_BIRTH AS ACTOR_BIRTH, AC.ACTOR_GENDER AS ACTOR_GENDER")
+			.append(StringUtils.LF)
+			.append("FROM MOVIE_ACTOR MA")
+			.append(StringUtils.LF)
+			.append("JOIN")
+			.append(StringUtils.LF)
+			.append("MOVIE MV ON MV.MOVIE_CODE = MA.MOVIE_CODE")
+			.append(StringUtils.LF)
+			.append("JOIN")
+			.append(StringUtils.LF)
+			.append("ACTOR AC ON AC.ACTOR_CODE = MA.ACTOR_CODE")
+			.append(StringUtils.LF)
+			.append("WHERE MOVIE_TITLE = ")
+			.append("\'" + movieTitle + "\'")
+			.toString();
 
-		/*
-		 * 영화 이름으로 검색할 것이기 때문에 movie 테이블에서 해당 영화이름을 가진 영화의 코드를 받아온 뒤, movie_actor 테이블에서 해당 영화코드에 할당된 actor_code를
-		 * 받아오고, 그것을 기반으로 actor테이블에서 배우 정보를 얻어온다.
-		 * subquery를 이용했다.
-		 * */
-		String query = "select actor_code, actor_name,actor_gender, actor_birth from actor where actor_code in "
-			+ "(select actor_code from movie_actor where movie_code in " + "(select movie_code from movie where movie_title='" + movieTitle
-			+ "'))";
-
-		try {
-			pstm = conn.prepareStatement(query);
-			result = pstm.executeQuery();
-			while (result.next()) {
-				//				actorByMovie.add(new ActorDTO(result.getString("actor_code"), result.getString("actor_name"), result.getString("actor_gender"),
-				//						result.getString("actor_birth")));
-			}
-			if (actorByMovie.size() == 0) {
-				System.out.println("그런 영화가 없거나 관리자가 배우정보를 등록하지 않았습니다.");
-				return;
-			}
-			System.out.println("배우이름\t배우생일\t배우성별");
-			for (int i = 0; i < actorByMovie.size(); i++) {
-				//				System.out.println(actorByMovie.get(i).actorName + "\t" + actorByMovie.get(i).actorBirth + "\t" + actorByMovie.get(i).actorGender);
-
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+		List<ActorDTO> actors = JDBCManager.getInstance().queryForList(query, ActorDTO.class);
+		if (actors.size() == 0) {
+			System.out.println("영화정보가 없거나 배우정보가 존재하지 않습니다.");
+		} else {
+			actors.stream().forEach(actorDTO -> {
+				System.out.println("배우명 : " + actorDTO.getActorName() + "\t" + "성별 : " + actorDTO
+					.getActorGender() + " \t " + "생년월일 : " + actorDTO.getActorBirth());
+			});
 		}
-		try {
-			result.close();
-			pstm.close();
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
 	}
 }
--- dbpro/src/test/java/dao/ActorDAOTest.java
+++ dbpro/src/test/java/dao/ActorDAOTest.java
@@ -1,42 +1,21 @@
 package dao;
 
 import dao.admin.ActorDAO;
-import dto.admin.ActorDTO;
-import oracle.connect.JDBCManager;
-import org.apache.commons.lang3.StringUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ActorDAOTest {
 
+	@Ignore
 	@Test
 	public void getActorsTest() {
 		ActorDAO.getInstance().list();
 	}
 
+	@Ignore
 	@Test
 	public void getActorByMovieTitleTest() {
-		final String movieTitle = "'겟 아웃'";
-		final String query = new StringBuilder(
-			"SELECT AC.ACTOR_CODE AS ACTOR_CODE, AC.ACTOR_NAME AS ACTOR_NAME, AC.ACTOR_BIRTH AS ACTOR_BIRTH, AC.ACTOR_GENDER AS ACTOR_GENDER")
-			.append(StringUtils.LF)
-			.append("FROM MOVIE_ACTOR MA")
-			.append(StringUtils.LF)
-			.append("JOIN")
-			.append(StringUtils.LF)
-			.append("MOVIE MV ON MV.MOVIE_CODE = MA.MOVIE_CODE")
-			.append(StringUtils.LF)
-			.append("JOIN")
-			.append(StringUtils.LF)
-			.append("ACTOR AC ON AC.ACTOR_CODE = MA.ACTOR_CODE")
-			.append(StringUtils.LF)
-			.append("WHERE MOVIE_TITLE = ")
-			.append(movieTitle)
-			.toString();
-
-		JDBCManager
-			.getInstance()
-			.queryForList(query, ActorDTO.class)
-			.forEach(actorDTO -> System.out.println(actorDTO.toString()));
+		ActorDAO.getInstance().getActor("겟 아웃");
 	}
 
 }
