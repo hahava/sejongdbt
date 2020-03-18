ffbf57de32c2fc1cdf08f6702aa16bf53f16eee3
hahava <hahava5696@gmail.com>
2020-03-18 18:11:28 +0900

    Refactor movie actor method

--- dbpro/src/main/java/dao/admin/MovieActorDAO.java
+++ dbpro/src/main/java/dao/admin/MovieActorDAO.java
@@ -1,64 +1,33 @@
 package dao.admin;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
 import dto.admin.MovieActorDTO;
 import oracle.connect.JDBCManager;
 
-public class MovieActorDAO implements DAO {
-	private Connection getConnection() {
-		JDBCManager manager = new JDBCManager();
-		String oracleId = "s15010924";
-		String passwd = "s15010924";
-		int port = 1521;
-
-		Connection conn = manager.connect(oracleId, passwd, port);
-		return conn;
-	}
+public class MovieActorDAO {
 
 	private MovieActorDAO() {
-
 	}
 
-	// 객체 생성한다.
-	private static MovieActorDAO instance = new MovieActorDAO();
+	private static MovieActorDAO instance;
 
-	// 객체를 변환한다.
 	public static MovieActorDAO getInstance() {
+		if (instance == null) {
+			instance = new MovieActorDAO();
+		}
 		return instance;
 	}
 
 	public void list() {
-		ArrayList<MovieActorDTO> arrayList = new ArrayList<>();
+		final String query = "select " +
+			"ACTOR_CODE, " +
+			"MOVIE_CODE, " +
+			"ACTOR_ROLE " +
+			"	from " +
+			"MOVIE_ACTOR";
 
-		Connection conn = getConnection();
-		PreparedStatement pstm = null;
-		ResultSet result = null;
-		String query = "select ACTOR_CODE, MOVIE_CODE, ACTOR_ROLE from MOVIE_ACTOR";
-
-		try {
-			pstm = conn.prepareStatement(query);
-			result = pstm.executeQuery();
-			while (result.next()) {
-				arrayList.add(new MovieActorDTO(result.getString("ACTOR_CODE"), result.getString("MOVIE_CODE"), result.getString("ACTOR_ROLE")));
-			}
-		} catch (SQLException e1) {
-			System.out.println(e1);
-		}
-
-		try {
-			result.close();
-			pstm.close();
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		for (int i = 0; i < arrayList.size(); i++) {
-			System.out.println(arrayList.get(i).toString());
-		}
+		JDBCManager
+			.getInstance()
+			.queryForList(query, MovieActorDTO.class)
+			.forEach(movieActorDTO -> System.out.println(movieActorDTO.toString()));
 	}
 }
