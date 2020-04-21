cf8188e3c3509e026c26a2a00029486ada4d9b00
hahava <hahava5696@gmail.com>
2020-04-21 21:43:09 +0900

    Add init and destory method in main class

--- dbpro/src/main/java/kr/ac/sejong/main/Main.java
+++ dbpro/src/main/java/kr/ac/sejong/main/Main.java
@@ -3,10 +3,13 @@ package kr.ac.sejong.main;
 import kr.ac.sejong.auth.Account;
 import kr.ac.sejong.menu.MainView;
 import kr.ac.sejong.menu.MenuSelector;
+import kr.ac.sejong.util.PropertiesWrapper;
 import org.apache.commons.lang3.StringUtils;
-import kr.ac.sejong.util.ConsoleUtil;
 
 import java.io.IOException;
+
+import static kr.ac.sejong.util.ConsoleUtil.close;
+import static kr.ac.sejong.util.ConsoleUtil.readString;
 
 /**
  * @author kalin
@@ -14,21 +17,42 @@ import java.io.IOException;
  */
 public class Main {
 
+	public static void initialize() {
+		try {
+			Class.forName(PropertiesWrapper.getInstance().getProperty("jdbc.driver"));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
+		initialize();
 
 		while (Account.isLogin() == false) {
-			String id = ConsoleUtil.readString("아이디를 입력해주세요.");
-			String pw = ConsoleUtil.readString("패스워드를 입력해주세요.");
+			String id = readString("아이디를 입력해주세요.");
+			String pw = readString("패스워드를 입력해주세요.");
 			Account.doLogin(id, pw);
 		}
 
 		while (true) {
 			MainView.show();
-			String requestCode = ConsoleUtil.readString("원하는 메뉴를 선택해주세요");
-			if (StringUtils.equals("프로그램 종료", requestCode)) {
-				System.exit(0);
+			String requestMenu = readString("원하는 메뉴를 선택해주세요");
+
+			if (StringUtils.equals("프로그램 종료", requestMenu)) {
+				break;
 			}
-			MenuSelector.getInstance().select(requestCode);
+
+			MenuSelector.getInstance().select(requestMenu);
+		}
+		destroy();
+	}
+
+	public static void destroy() {
+		try {
+			close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
+
 }
